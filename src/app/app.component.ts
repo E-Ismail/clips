d7fbcdad2a79e9ee72348abcfa1faabc4b122b28
Ech-Cherrate Ismail <echcherrate.ismail@gmail.com>
2025-08-17 20:24:10 +0200

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ModalService } from './services/modal.service';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: false,
 })
 export class AppComponent {
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -2,27 +2,21 @@ import { Component, Input, OnInit, ElementRef } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
-    selector: 'app-modal',
-    templateUrl: './modal.component.html',
-    styleUrls: ['./modal.component.css'],
-    standalone: false
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+  standalone: false,
 })
 export class ModalComponent implements OnInit {
-  @Input() modalID = ''
+  @Input() modalID = '';
 
-  constructor(
-    public modal: ModalService, 
-    public el: ElementRef
-  ) { 
-    
-  }
+  constructor(public modal: ModalService, public el: ElementRef) {}
 
   ngOnInit(): void {
-    document.body.appendChild(this.el.nativeElement)
+    document.body.appendChild(this.el.nativeElement);
   }
 
   closeModal() {
-    this.modal.toggleModal(this.modalID)
+    this.modal.toggleModal(this.modalID);
   }
-
 }
